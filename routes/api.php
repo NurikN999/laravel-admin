e75a693a4cb2d234865f61f84f5d7c1f68ec092f
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2023-01-24 15:12:34 +0500

    feat: added user api methods

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\AuthController;
+use \App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,6 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user'] );
     Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('users', UserController::class);
 });
