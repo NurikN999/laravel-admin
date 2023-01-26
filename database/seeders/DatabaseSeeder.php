e7f1b9b8e124b3720b18432886b6358239875992
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2023-01-26 18:50:18 +0500

    feat: changed DB seeder and added Roles Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Role::factory()->create([
+            'name' => 'Admin'
+        ]);
+
+        Role::factory()->create([
+            'name' => 'Editor'
+        ]);
+
+        Role::factory()->create([
+            'name' => 'Viewer'
+        ]);
          \App\Models\User::factory(20)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'firstname' => 'Admin',
+             'lastname' => 'Admin',
+             'email' => 'admin@admin.com',
+             'role_id' => 1
+         ]);
+
+        \App\Models\User::factory()->create([
+            'firstname' => 'Editor',
+            'lastname' => 'Editor',
+            'email' => 'editor@editor.com',
+            'role_id' => 2
+        ]);
+
+        \App\Models\User::factory()->create([
+            'firstname' => 'Viewer',
+            'lastname' => 'Viewer',
+            'email' => 'viewer@viewer.com',
+            'role_id' => 3
+        ]);
     }
 }
